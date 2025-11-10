dc277efcfa3e760f8653c64e859ddc4dbafde9b2
MSII <MSII@DAT>
2025-11-10 13:20:05 +0700

    Add project files.

---
+++ ScentoryApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ScentoryApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
