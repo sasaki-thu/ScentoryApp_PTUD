a393de5a958721fa6f893aa3fc7341f92f63c672
31231026098@ueh.edu.vn <31231026098@ueh.edu.vn>
2025-11-21 19:24:29 +0700

    Kì chỉnh menu ng dùng

--- ScentoryApp/Controllers/HomeController.cs
+++ ScentoryApp/Controllers/HomeController.cs
@@ -47,6 +47,11 @@ namespace ScentoryApp.Controllers
             return View();
         }
 
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
