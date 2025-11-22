38a2facd261b5f25fc1c1099a0e75abba15cfb13
31231027140@ueh.edu.vn <huytran.31231027140@st.ueh.edu.vn>
2025-11-22 22:43:33 +0700

    Le Huy lam trang gio hang 22/11

---
+++ ScentoryApp/Controllers/CartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScentoryApp.Models;
+using System.Diagnostics;
+namespace ScentoryApp.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
