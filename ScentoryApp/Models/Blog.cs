c8af3dc4ea409dde3df5b22f19a73df8d0c77333
31231027424@ueh.edu.vn <31231027424@ueh.edu.vn>
2025-11-29 15:24:22 +0700

    Rem tạm thời update Blog 29/11

---
+++ ScentoryApp/Models/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScentoryApp.Models;
+
+public partial class Blog
+{
+    public string IdBlog { get; set; } = null!;
+
+    public string TenBlog { get; set; } = null!;
+
+    /// <summary>
+    /// HDSD/Kiến thức/Mẹo/Sản phẩm/Thương hiệu
+    /// </summary>
+    public string? DanhMucBlog { get; set; }
+
+    public string NoiDung { get; set; } = null!;
+
+    public byte[]? AnhBlog { get; set; }
+
+    public DateTime ThoiGianTaoBlog { get; set; }
+
+    public DateTime? ThoiGianCapNhatBlog { get; set; }
+}
